2d84f615d5a83a433fd1359967cac56b5c575da1
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-10-09 22:12:29 -0400

    model creation

--- firedata/models.py
+++ firedata/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class MunicipioQUeimada(models.Model):
+    municipio = models.CharField(max_length=100, primary_key=True)
+    data_hora = models.DataTimeField()
+    estado = models.CharField(max_length=100)
+    bioma = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    frp = models.FloatField()
+    precipita = models.FloatField(null=True, blank=True)
+    dias_sem_chuva = models.FloatField()
+
+    def __str__(self):
+        return f'{self.municipio} - {self.data_hora}'
